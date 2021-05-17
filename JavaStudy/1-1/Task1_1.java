45e7eedf93204d58fa75114a690ee4cdd2ee4553
Akarin0001 <83531860+Akarin0001@users.noreply.github.com>
2021-05-17 20:51:45 +0900

    　データ型・文字列クラス　課題
    Task1_1.javaの作成

---
+++ JavaStudy/1-1/Task1_1.java
@@ -0,0 +1,25 @@
+public class Task1_1 {
+
+    public static void main(String[]arge) {
+
+        int hogeInt = 50;
+
+        String hogeString = "テスト";
+
+        // double型の hogeDouble という名の変数を宣言し、値０．２で初期化
+        double hogeDouble = 0.2;
+        
+        hogeInt = 100;
+
+        hogeString = "合格";
+
+        System.out.println(hogeInt);
+
+        // 変数hogeStringの値を表示
+        System.out.println(hogeString);
+
+        System.out.println(hogeDouble);
+
+ 
+    }
+}
