99ecea8bfe2468d726c992083c861c31c69fce70
Akarin0001 <83531860+Akarin0001@users.noreply.github.com>
2021-05-30 02:10:21 +0900

    課題提出
    Java Task1_2

---
+++ JavaStudy/1-2/Task1_2.java
@@ -0,0 +1,26 @@
+public class Task1_2 {
+
+    public static void main(String[] args) {
+
+        String[] country ={"JAPAN", "AMERICA", "KOREA"};
+
+        System.out.println(country.length);
+
+        String[] strArray = {"りんご", "もも", "ぶどう"};
+
+        System.out.println(strArray[1]);
+
+        //intArray という名のint型配列を宣言
+        int[]intArray = {10, 20, 30, 40, 50 };
+        
+        //1と4の要素の値を表示
+        System.out.println(intArray[1] + intArray[4]);
+
+        
+
+        
+
+        
+
+    }
+}
---
+++ Task1_2.java
@@ -0,0 +1,26 @@
+public class Task1_2 {
+
+    public static void main(String[] args) {
+
+        int[] country ={"JAPAN", "AMERICA", "KOREA"};
+
+        System.out.println("3"+ country.length);
+
+        String[] strArray = {"りんご", "もも", "ぶどう"};
+
+        strArray[1] = "もも";
+
+        //intArray という名のint型配列を宣言
+        int[]intarray = {10, 20, 30, 40, 50 };
+        
+        //1と4の要素の値を表示
+        System.out.println(intArray[1] + intArray[4]);
+
+        
+
+        
+
+        
+
+    }
+}
