489647593a883bf621ac0dab51fb8124bb166aed
Akarin0001 <83531860+Akarin0001@users.noreply.github.com>
2021-05-30 12:09:58 +0900

    演算子
    課題提出

---
+++ JavaStudy/1-3/Task1_3.java
@@ -0,0 +1,29 @@
+public class Task1_3 {
+
+    public static void main(String[] args) {
+
+        int plus = 5 + 10;
+
+        int minus = 20 - 7;
+
+        //10と2を乗算した値を、変数multiplyに代入している。
+        int multiply = 10 * 2;
+
+        int remi = 20 % 6;
+
+
+        //有楽町という文字列を、変数townに代入している
+        //綿という文字列を、変数lineに代入している。
+        String town = "有楽町";
+        String line = "綿";
+
+        String train = town + line;
+
+        System.out.println(plus);
+        System.out.println(minus);
+        System.out.println(multiply);
+        System.out.println(remi);
+        System.out.println(train);
+ 
+    }
+}
