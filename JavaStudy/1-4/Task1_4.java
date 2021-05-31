93a1969d33e591e5ba7fa2b8032ab1ecfa90afb4
Akarin0001 <83531860+Akarin0001@users.noreply.github.com>
2021-05-31 23:36:09 +0900

    Task1－2　再
    課題

---
+++ JavaStudy/1-4/Task1_4.java
@@ -0,0 +1,34 @@
+public class Task 1_4 {
+
+    private static final String USER_NAME = Akaridon;
+    private static final String USER_PASSWORD = 123456;
+
+    private static final String CONST_MSG_SUCCESS = "ログイン成功です。";
+    private static final String CONST_MSG_ERROR_NAME = "名前に誤りがあります。";
+    private static final String COUNT_MSG_ERROR_PASS = "パスワードに誤りがあります。";
+    private static final String CONST_MSG_ERROR_INPUT = "入力情報に誤りがあります。";
+
+    public static void main(String[] arge) {
+
+        String name = "alice";
+        String pass = " alice123";
+
+        if (name = akaridon && pass = 123456){
+            System.out.println(ログイン成功です。);
+        }
+
+        if (name = akaridon != pass = 123456){
+            System.out.println(パスワードに誤りがあります。);
+        }
+
+        if (pass = 123456 != name = akaridon){
+            System.out.println(名前に誤りがあります。);
+        }
+
+        else {
+            System.out.println(入力情報に誤りがあります。);
+        }
+
+
+    }
+}
