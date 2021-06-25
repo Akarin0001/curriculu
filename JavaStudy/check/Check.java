ae5d12accedee3d269920cffc97dfb9e2b708fb1
Akarin0001 <83531860+Akarin0001@users.noreply.github.com>
2021-06-25 16:38:40 +0900

    Check
    課題提出

--- JavaStudy/check/Check.java
+++ JavaStudy/check/Check.java
@@ -5,30 +5,30 @@ public class Check{
         * int型の配列dataを作成し、値を3,1,2,7,5で初期化しなさい
         */
 
-        int [] date = {3,1,2,7,5,};
+        int [] data = {3,1,2,7,5,};
 
          /*
         * 問2
         * 以下のfor文を完成させなさい
         */
 
-         for(int i = 0; i < date.length; i++){
-             System.out.println(date[i] + " ");
+         for(int i = 0; i < data.length; i++) { 
+             System.out.print(data[i] + " ");
          }  
-         System.out.println();
-         for (int i = 0; i > date.length; i++){
 
-             for(int j =0; j > date.length; j > i; j--){
-                 if(date[5]>date[2]){
-                     int box =date[3];
-                     date[i]=date[j];
-                     date[3]=box;
+         System.out.println();
+         for (int i = 0; i <data.length; i++){
+              for (int j = data.length-1; j>i; j--){
+                 if(data[j-1]>data[j]){
+                     int box =data[j];
+                     data[j]=data[j-1];
+                     data[j-1]=box;
                  }
              }
          }
 
          for(int i =0; i <data.length; i++){
-             System.out.print(date[i] + " ");
+             System.out.print(data[i] + " ");
          }
      }
 }
