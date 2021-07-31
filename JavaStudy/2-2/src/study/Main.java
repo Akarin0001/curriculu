050c9c29c0c92fb2411035e98ca783079d068731
Akarin0001 <83531860+Akarin0001@users.noreply.github.com>
2021-07-31 14:45:45 +0900

    2－2課題
    2－2課題

---
+++ JavaStudy/2-2/src/study/Main.java
@@ -0,0 +1,21 @@
+package study;
+
+public class Main {
+	public static void main(String [] args) {
+		  // ① 以下のルールに従いUserクラスのインスタンスを生成してください。
+        // 変数名:「user」
+		User user  = new User("hoge",123, "123adc");
+		user.printAccountInfo();
+		
+		}
+	
+	
+	
+		
+		
+	
+		
+		 // ② ①のインスタンス変数より、アカウント情報をコンソール出力するメソッドを呼び出しなさい。
+
+
+}
